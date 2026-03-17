283c68472960e4b86202d9837460b0ff29e7b590
kilani olamilekan <programming@email.com>
2026-03-17 13:29:37 +0100

    added the font family volkron

--- src/app/components/themeRegistry.tsx
+++ src/app/components/themeRegistry.tsx
@@ -1,14 +1,20 @@
-'use client';
+"use client";
 
-import * as React from 'react';
-import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+import * as React from "react";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 
-const theme = createTheme();
+const theme = createTheme({
+  typography: {
+    fontFamily: "'Vollkorn', serif",
+  },
+});
 
-export default function ThemeRegistry({ children }: { children: React.ReactNode }) {
-  
-
+export default function ThemeRegistry({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <AppRouterCacheProvider>
       <ThemeProvider theme={theme}>
